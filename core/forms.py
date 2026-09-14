9426f12f5b2c4986f9c74b69523d684994bdbf40
Ibrahim Guilherme <guilhermeibrahim08@gmail.com>
2026-09-14 08:31:09 -0300

    Implementação do Crispy forms ainda com erros

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,25 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, HTML, Div
+
 from .models import Noticia
 
 class NoticiaForm(forms.ModelForm):
     class Meta:
         model = Noticia
-        fields = "__all__"
+        fields = ["titulo", "imagem", "conteudo"]
+        widgets = {
+            "titulo": forms.TextInput(attrs={"placeholder": "Título de notícia"}),
+        }
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            self.helper = FormHelper()
+            self.helper.form_tag = False
+            self.helper.layout = Layout(
+                Field("titulo"),
+                Field("imagem"),
+                HTML("<label class='form-label mt-2'>Conteúdo</label>"),
+                Div(Field("conteudo"), css_class="mb-3"),
+            )
