59a90c70ca3b0dda4b6798482ec9debc540c4001
Ibrahim Guilherme <guilhermeibrahim08@gmail.com>
2026-09-24 14:57:23 -0300

    Adicionado cadastro e login do napne no portal/urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('noticia/<int:id>/', views.detalhe, name='detalhe'),
     path('enviar-pei/', views.enviar_pei, name='enviar_pei'),
     path('pesquisar_noticias/', views.pesquisar_noticias, name='pesquisar_noticias'),
+    path('napne/cadastro/', views.cadastro_napne, name='cadastro_napne'),
+    path('napne/login/', views.login_napne, name='login_napne'),
 ]
